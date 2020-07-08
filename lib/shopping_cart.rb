134f50bc5c5ac5f336c2cffe1a0c3e95a42e3559
Nico Rithner <nicorithner@gmail.com>
2020-07-08 10:07:18 -0600

    Add it can add product method and test, and it has cart details

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -5,5 +5,13 @@ class ShoppingCart
         @capacity = capacity[0..1].to_i
         @products = []
     end
+    
+    def add_product(product)
+        products << product
+    end
+
+    def details
+      cart_details = {name: @name, capacity: @capacity}
+    end
 
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -19,5 +19,16 @@ class ShoppingCartTest < Minitest::Test
     assert_equal [], cart.products
   end
 
-  
+  def test_it_can_add_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+# binding.pry
+    assert_equal [product1, product2], cart.products
+    assert_equal cart_details = {name: "King Soopers", capacity: 30}, cart.details
+  end
+
 end
